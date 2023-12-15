269db7d0248942fd12b59f704f86f2af1246750c
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-12-15 17:07:25 +0800

    Update twrp_begonia.mk

--- twrp_begonia.mk
+++ twrp_begonia.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base_aosp.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from begonia device
 $(call inherit-product, device/xiaomi/begonia/device.mk)
